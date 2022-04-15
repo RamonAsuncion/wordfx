3e84ea464c11096267f330388c2589818538c121
Pedro Passos <pccp001@bucknell.edu>
2022-04-15 13:17:09 -0400

    fixed one method

--- src/main/java/main/view/WordleView.java
+++ src/main/java/main/view/WordleView.java
@@ -142,8 +142,8 @@ public class WordleView {
         this.guessEval = new GuessEvaluator(this.wordleModel.getSecretWord(), s.toString());
 
         // Make sure the word is a valid word
-        if(this.wordleModel.getReader().isWordInSet(s.toString().toLowerCase()) == true) {
-            String evaluation = this.guessEval.analyzeGuess(s.toString());
+        if(this.wordleModel.getReader().isWordInSet(s.toString().toLowerCase())) {
+            String evaluation = this.guessEval.analyzeGuess();
             performScreenAnimation(evaluation, s.toString());
             // If the user gets the right word.
             if (evaluation.equals("*****")) {
