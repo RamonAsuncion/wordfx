b815d0a51a0a49cbf0b6fc8556832d96e9319c7f
Alvin Huynh <aah016@bucknell.edu>
2022-04-17 12:21:52 -0400

    created controller for header buttons

---
+++ src/main/java/main/controller/HeaderController.java
@@ -0,0 +1,20 @@
+/* ***************************************** * CSCI205 -Software Engineering and Design
+ * Spring2022* Instructor: Prof. Brian King
+ *
+ * Name: Alvin Huynh
+ * Section: 11 AM
+ * Date: 4/17/22
+ * Time: 12:14 PM
+ *
+ * Project: csci205_final_project
+ * Package: main.controller
+ * Class: HeaderController
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main.controller;
+
+public class HeaderController {
+}
