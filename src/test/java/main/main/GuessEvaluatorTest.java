fbd3d17d7f75a5fbd2195475905915b211f31086
Pedro Passos <pccp001@bucknell.edu>
2022-05-03 23:04:24 -0400

    JUnit Testing

--- src/test/java/main/main/GuessEvaluatorTest.java
+++ src/test/java/main/main/GuessEvaluatorTest.java
@@ -7,9 +7,9 @@
 package main.main;
 
 import javafx.application.Application;
+import main.model.GameState;
 import main.model.WordleModel;
 import main.view.WordleView;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,33 +17,81 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GuessEvaluatorTest {
 
-    private GuessEvaluator evaluator;
-    private WordleModel wordleModel;
-    private WordleView wordleView;
+    private GuessEvaluator evaluator3;
+    private WordleModel wordleModel3;
+    private WordleView wordleView3;
+
+    private GuessEvaluator evaluator4;
+    private WordleModel wordleModel4;
+    private WordleView wordleView4;
+
+    private GuessEvaluator evaluator5;
+    private WordleModel wordleModel5;
+    private WordleView wordleView5;
 
     @BeforeEach
     void setUp() {
         Application.launch(main.main.WordleMain.class);
+        wordleModel3 = new WordleModel(3);
+        wordleView3 = new WordleView(wordleModel3);
+        evaluator3 = new GuessEvaluator(wordleModel3, wordleView3, "row");
+
+//        wordleModel4 = new WordleModel(4);
+//        wordleView4 = new WordleView(wordleModel4);
+//        evaluator4 = new GuessEvaluator(wordleModel4, wordleView4, "time");
+//
+//        wordleModel5 = new WordleModel(5);
+//        wordleView5 = new WordleView(wordleModel5);
+//        evaluator5 = new GuessEvaluator(wordleModel5, wordleView5, "state");
+
     }
 
-//    @AfterAll
-//    void tearDown() {
-//    }
+    void guessCorrect() {
+        String analysis3 = evaluator3.analyzeGuess("row", 3);
+        assertEquals(analysis3, "***");
+
+//        String analysis4 = evaluator4.analyzeGuess("time", 4);
+//        assertEquals(analysis4, "****");
+//
+//        String analysis5 = evaluator5.analyzeGuess("state", 5);
+//        assertEquals(analysis5, "*****");
+    }
+
+    void guessWrong() {
+        String analysis1 = evaluator3.analyzeGuess("tab", 3);
+        assertEquals(analysis1, "---");
+        String analysis2 = evaluator3.analyzeGuess("rap", 3);
+        assertEquals(analysis2, "*--");
+        String analysis3 = evaluator3.analyzeGuess("the", 3);
+        assertEquals(analysis3, "---");
+        String analysis4 = evaluator3.analyzeGuess("for", 3);
+        assertEquals(analysis4, "-*+");
+        String analysis5 = evaluator3.analyzeGuess("two", 3);
+        assertEquals(analysis5, "-++");
+        String analysis6 = evaluator3.analyzeGuess("win", 3);
+        assertEquals(analysis6, "+--");
+    }
 
     @Test
     void analyzeGuess() {
-        wordleModel = new WordleModel(3);
-        wordleView = new WordleView(wordleModel);
-        evaluator = new GuessEvaluator(wordleModel, wordleView, "row");
-        String analysis = evaluator.analyzeGuess("row", 3);
-        assertEquals(analysis, "***");
+        guessCorrect();
     }
 
     @Test
     void loserUser() {
+        guessWrong();
+        assertEquals(wordleModel3.getGameState(), GameState.NEW_GAME);
+        assertEquals(wordleModel3.getCurrentWinStreak(), 0);
     }
 
     @Test
     void winnerUser() {
+        guessCorrect();
+        assertEquals(wordleModel3.getGameState(), GameState.NEW_GAME); //due to closing app (stays as game winner before closing)
+        assertEquals(wordleModel3.getCurrentWinStreak(), 1);
+//        assertEquals(wordleModel4.getGameState(), GameState.NEW_GAME); //due to closing app (stays as game winner before closing)
+//        assertEquals(wordleModel4.getCurrentWinStreak(), 1);
+//        assertEquals(wordleModel5.getGameState(), GameState.NEW_GAME); //due to closing app (stays as game winner before closing)
+//        assertEquals(wordleModel5.getCurrentWinStreak(), 1);
     }
 }
