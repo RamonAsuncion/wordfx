d38ece12dcf95b4f159a761fa27a7705f95d3f9c
Ramon Asuncion <asuncionbatista@gmail.com>
2022-05-03 22:39:37 -0400

    urmom

--- src/test/java/main/main/GuessEvaluatorTest.java
+++ src/test/java/main/main/GuessEvaluatorTest.java
@@ -4,34 +4,46 @@
  * regular players and the game seems to be working fine (We wish we could have fixed this on time).
  */
 
-//package main.main;
-//
-//import main.model.WordleModel;
-//import main.view.WordleView;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class GuessEvaluatorTest {
-//
-//    private GuessEvaluator evaluator;
-//    private WordleModel wordleModel;
-//    private WordleView wordleView;
-//
-//    @Test
-//    void analyzeGuess() {
-//        wordleModel = new WordleModel(3);
-//        wordleView = new WordleView(wordleModel);
-//        evaluator = new GuessEvaluator(wordleModel, wordleView, "row");
-//        String analysis = evaluator.analyzeGuess("row", 3);
-//        assertEquals(analysis, "*****");
+package main.main;
+
+import javafx.application.Application;
+import main.model.WordleModel;
+import main.view.WordleView;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuessEvaluatorTest {
+
+    private GuessEvaluator evaluator;
+    private WordleModel wordleModel;
+    private WordleView wordleView;
+
+    @BeforeEach
+    void setUp() {
+        Application.launch(main.main.WordleMain.class);
+    }
+
+//    @AfterAll
+//    void tearDown() {
 //    }
-//
-//    @Test
-//    void loserUser() {
-//    }
-//
-//    @Test
-//    void winnerUser() {
-//    }
-//}
+
+    @Test
+    void analyzeGuess() {
+        wordleModel = new WordleModel(3);
+        wordleView = new WordleView(wordleModel);
+        evaluator = new GuessEvaluator(wordleModel, wordleView, "row");
+        String analysis = evaluator.analyzeGuess("row", 3);
+        assertEquals(analysis, "***");
+    }
+
+    @Test
+    void loserUser() {
+    }
+
+    @Test
+    void winnerUser() {
+    }
+}
